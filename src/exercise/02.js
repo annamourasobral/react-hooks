df04b2b1616c2a746c79f5ec749179c742146dc8
Anna Moura Sobral <anna.moura@qatium.com>
2023-10-26 19:36:59 +0200

    lazy useEffect and custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,11 +6,23 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') ?? initialName,
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  const useLocalStorageState = (stateVariable, state) => {
+    React.useEffect(() => {
+      window.localStorage.setItem(
+        stateVariable,
+        JSON.parse(JSON.stringify(state)),
+      )
+    }, [state, stateVariable])
+  }
+
+  useLocalStorageState('name', name)
 
   function handleChange(event) {
     setName(event.target.value)
